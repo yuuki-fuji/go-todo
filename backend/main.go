f31a5797673d258728da05d3590e78cf7108ed33
yuuki-fuji <yuukikun666@gmail.com>
2025-12-14 10:07:31 +0900

    簡単なレスポンスを確認

---
+++ backend/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
+	log.Println("server start :8880")
+	log.Fatal(http.ListenAndServe(":8880", nil))
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/yuuki-fuji/go-todo
+
+go 1.21.13
